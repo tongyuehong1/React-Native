9416bfab21070bbd2ba3723b3804ee5919b51b0b
liuqiiiiiii <32216634+liuqiiiiiii@users.noreply.github.com>
2018-05-06 16:40:47 +0800

    add map page and the related configuration.

--- traffic/app/pages/Main.js
+++ traffic/app/pages/Main.js
@@ -1,51 +1,43 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { View, Text, Picker } from 'react-native';
+import { MapView } from 'react-native-amap3d';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const coordinate = {
+  latitude: 39.706901,
+  longitude: 116.397972,
+};
 
-export default class Main extends Component {
+export default class MapTypesExample extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { state, setParams } = navigation;
+    state.params = state.params || { mapType: 'standard' };
+    const props = {
+      mode: 'dropdown',
+      style: { width: 100 },
+      selectedValue: state.params.mapType,
+      onValueChange: mapType => setParams({ mapType }),
+    };
+    return {
+      title: '地图模式',
+      headerRight: (
+        <Picker {...props}>
+          <Picker.Item label="标准" value="standard" />
+          <Picker.Item label="卫星" value="satellite" />
+          <Picker.Item label="导航" value="navigation" />
+          <Picker.Item label="夜间" value="night" />
+          <Picker.Item label="公交" value="bus" />
+        </Picker>
+      ),
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <MapView.Marker image="flag" coordinate={coordinate}>
+        <View>
+          <Text>自定义信息窗体</Text>
+        </View>
+      </MapView.Marker>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
