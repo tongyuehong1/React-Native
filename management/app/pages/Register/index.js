9c3b8daa933193b40d327e029d87dfdd0bc2e55e
liuqiiiiiii <32216634+liuqiiiiiii@users.noreply.github.com>
2018-05-14 20:44:37 +0800

    modify register page content.

--- management/app/pages/Register/index.js
+++ management/app/pages/Register/index.js
@@ -3,42 +3,84 @@ import {
   StyleSheet,
   Text,
   View,
-  Image,
 } from 'react-native';
+import { Madoka } from 'react-native-textinput-effects';
 
 import Layout from '../../res/dimensions';
 
-export default class Register extends Component {
+export default class Information extends Component {
   static navigationOptions = {
     header: null,
   }
   render() {
     return (
-      <View style={styles.global}>
-        <Image
-          style={styles.image}
-          source={{ uri: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1526202568243&di=8a8283400066ebee3307baf127f4a441&imgtype=0&src=http%3A%2F%2Fimg.itlun.cn%2Fuploads%2Fallimg%2F170109%2F1-1F109194Z1.jpg" }}
-        />
-        <Text style={styles.font}>你他妈注册个球</Text>
-        <Text style={styles.font}>二逼</Text>
+      <View>
+        <View>
+          <View style={styles.input}>
+            <Text style={styles.inputFont}>班级</Text>
+            <Madoka
+              style={{ width: Layout.Width(300), marginLeft: Layout.Width(40) }}
+              label="计算机14k2班"
+              // this is used as active and passive border color
+              borderColor="#EEE8AA"
+              labelStyle={{ color: '#808080' }}
+              inputStyle={{ color: '#f4a197' }}
+            />
+          </View>
+          <View style={styles.input}>
+            <Text style={styles.inputFont}>学号</Text>
+            <Madoka
+              style={{ width: Layout.Width(300), marginLeft: Layout.Width(40) }}
+              label="141903020702"
+              // this is used as active and passive border color
+              borderColor="#aee2c9"
+              labelStyle={{ color: '#808080' }}
+              inputStyle={{ color: '#f4a197' }}
+            />
+          </View>
+          <View style={styles.input}>
+            <Text style={styles.inputFont}>电话</Text>
+            <Madoka
+              style={{ width: Layout.Width(300), marginLeft: Layout.Width(40) }}
+              label="13833008838"
+              // this is used as active and passive border color
+              borderColor="#FA8072"
+              labelStyle={{ color: '#808080' }}
+              inputStyle={{ color: '#f4a197' }}
+            />
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  global: {
-    marginTop: Layout.Height(100),
+  personal: {
+    marginBottom: Layout.Height(10),
+    paddingTop: Layout.Height(60),
+  },
+  editor: {
+    marginTop: Layout.Height(20),
+    marginRight: Layout.Width(20),
+  },
+  avatar: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: Layout.Height(20),
   },
-  image: {
-    height: Layout.Height(300),
-    width: Layout.Width(300),
-    marginBottom: Layout.Height(100),
+  name: {
+    marginTop: Layout.Height(20),
   },
-  font: {
-    fontSize: 30,
-    lineHeight: 80,
+  nameStyle: {
+    fontSize: 20,
+    color: '#808080', //gray
+  },
+  input: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: Layout.Width(40),
+  },
+  inputFont: {
+    fontSize: 20,
   },
 });
